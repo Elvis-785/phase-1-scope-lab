2dcf5cf4380ac821397f0d40238234b5c02282af
Elvis-785 <kiruielvis3358@gmail.com>
2024-10-23 17:40:25 +0300

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+console.log(customerName);
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+const LeastFavoriteCustomer = "bob";
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "Marley";
+}
+
